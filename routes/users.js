26e76fe0a62ebc6ddd2916a3bef486dd90e8fc11
Ifan Ramadhani <ifan.lanterlite@gmail.com>
2017-07-14 18:55:32 +0700

    add multiple url

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 // router.get('/', function(req, res, next) {
-router.get('/:username', function(req, res, next) {
+router.get('/:username*/:date*', function(req, res, next) {
 	// res.send('respond with a resource');
   
   
@@ -10,9 +10,13 @@ router.get('/:username', function(req, res, next) {
 		// text: 'Hello, welcome to Astrajingga! :)'
 	// };
 	// return res.status(200).json(botPayload);
+	var dateTime = "default day"
+	if(req.params.date == "today")
+		dateTime = "2017/14/07"
 	
 	var botPayload = {
-		text: 'Hello' + req.param.username'
+		text: 'Hello ' + req.params.username,
+		date: dateTime,
 	};
 	return res.status(200).json(botPayload);
 });
