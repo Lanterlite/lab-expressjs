2dbac6d38c66bcf861fe82f70222dc162110ba44
Ifan Ramadhani <ifan.lanterlite@gmail.com>
2017-07-14 18:43:51 +0700

    add custom url

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  // res.send('respond with a resource');
-  		var botPayload = {
-			text: 'Hello, welcome to Astrajingga! :)'
-		};
-		
-		return res.status(200).json(botPayload);
+// router.get('/', function(req, res, next) {
+router.get('/:username', function(req, res, next) {
+	// res.send('respond with a resource');
+  
+  
+	// var botPayload = {
+		// text: 'Hello, welcome to Astrajingga! :)'
+	// };
+	// return res.status(200).json(botPayload);
+	
+	var botPayload = {
+		text: 'Hello' + req.param.username'
+	};
+	return res.status(200).json(botPayload);
 });
 
 module.exports = router;
