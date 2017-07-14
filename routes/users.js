2390c49daaceef8b413dd2a193aa1244adbec1b6
Ifan Ramadhani <ifan.lanterlite@gmail.com>
2017-07-14 18:27:51 +0700

    send json file

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('respond with a resource');
+  		var botPayload = {
+			text: 'Hello, welcome to Astrajingga! :)'
+		};
+		
+		return res.send(200).json(botPayload);
 });
 
 module.exports = router;
